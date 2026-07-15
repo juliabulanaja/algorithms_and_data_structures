c7997aa89c53152782f8c6fce99b382e00825de6
juliabulanaja <bulanaja.julia@gmail.com>
2026-07-15 13:38:37 -0700

    dijkstra algorithm fixed

--- Algorithms on Graphs/chapter_4(paths in graphs)/dijkstra.py
+++ Algorithms on Graphs/chapter_4(paths in graphs)/dijkstra.py
@@ -9,23 +9,27 @@ def dijkstra(start, end, adj_list):
     start -= 1
     end -= 1
 
-    cost = [float('inf')] * n
-    cost[start] = 0
+    destinations = [float('inf')] * n
+    destinations[start] = 0
 
     pq = PriorityQueue()
-    pq.put((cost[start], start))
+    pq.put((destinations[start], start))
 
     while not pq.empty():
-        c, u = pq.get()
+        d, u = pq.get()
 
         if u == end:
             break
 
+        if destinations[u] < d:
+            continue
+
         for v, w in adj_list[u]:
-            if cost[v] > cost[u] + w:
-                cost[v] = cost[u] + w
-                pq.put((cost[v], v))
-    return cost[end] if cost[end] < float('inf') else -1
+            if destinations[v] > destinations[u] + w:
+                destinations[v] = destinations[u] + w
+                pq.put((destinations[v], v))
+
+    return destinations[end] if destinations[end] < float('inf') else -1
 
 
 if __name__ == "__main__":
